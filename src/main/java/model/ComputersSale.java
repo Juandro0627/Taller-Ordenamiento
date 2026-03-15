a3b41beb1fd43db64c48fe77fa8e52e3e464dc45
Jhovan David V C <71225977+HOKAIH@users.noreply.github.com>
2026-03-14 20:26:21 -0500

    Actualizacion en algoritmos de ordenamiento de ComputersSale

--- src/main/java/model/ComputersSale.java
+++ src/main/java/model/ComputersSale.java
@@ -1,4 +1,7 @@
 package model;
+import java.util.Arrays;
+import java.util.Comparator;
+
 
 public class ComputersSale {
 
@@ -26,19 +29,59 @@ public class ComputersSale {
     }
 
     public void sortByProcessing(){
+        for(int i=0; i<computers.length;i++){
+            for(int j=0; j<computers.length - 1 - i ;j++){
 
+                double p1= computers[j].getProcessors()*computers[j].getProcessingSpeed();
+                double p2= computers[j+1].getProcessors()*computers[j+1].getProcessingSpeed();
+
+                if(p1 > p2){
+                    Computer temp = computers[j];
+                    computers[j] = computers[j+1];
+                    computers[j+1]= temp;
+                }
+            }
+        }
     }
 
     public void sortByBrandAndOs(){
+        Arrays.sort(computers, new Comparator<Computer>() {
 
+            @Override
+            public int compare(Computer c1, Computer c2) {
+
+                int brandCompare = c1.getBrand().compareTo(c2.getBrand());
+
+                if(brandCompare != 0){
+                    return brandCompare;
+                }
+
+                return Character.compare(c1.getOperatingSystem(), c2.getOperatingSystem());
+            }
+
+        });
     }
 
     public void sortByRam(){
+        Arrays.sort(computers, new Comparator<Computer>() {
 
+            @Override
+            public int compare(Computer c1, Computer c2) {
+                return Integer.compare(c1.getRam(), c2.getRam());
+            }
+
+        });
     }
 
     public void sortByBrandReverse(){
+        Arrays.sort(computers, new Comparator<Computer>() {
 
+            @Override
+            public int compare(Computer c1, Computer c2) {
+                return c2.compareTo(c1);
+            }
+
+        });
     }
      public Computer[] generateRandomComputers(int c){
 
